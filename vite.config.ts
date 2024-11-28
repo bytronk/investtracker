e1d32538969a7618e0743f348649083178be9f0f
bytronk <bytronk@gmail.com>
2024-11-29 00:58:21 +0100

    Solucion al problema de Not Found al acceder a rutas especificas.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "_redirects", // Archivo en la raíz del proyecto
+          dest: ".",         // Copiarlo a la raíz de la carpeta dist
+        },
+      ],
+    }),
+  ],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
